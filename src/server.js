6605346aaf533fa71927bf6a6bd06a201cc008d6
Jerônimo Santos Rodrigues <santosominorej@gmail.com>
2025-04-24 00:57:52 -0300

    :sparkles: feat: Ajustes Finais no server.js

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
-//
+// IMPORTANDO O EXPRESS
 import express from 'express';
 
-//
+// IMPORTANDO ROTAS DA API
 import inovacoes from './routes/inovacoes.route.js';
 import startups from './routes/startups.route.js';
 import comoAjudar from './routes/comoAjudar.route.js';
 
-//
+// INICIALIZANDO APP
 const app = express();
 
-//
+// DEFININDO PORTA NO SERVIDOR IRÁ RODAR
 const PORT = 3000;
 
-//
+// DEFININDO MIDDLEWARES
 app.use(express.json());
 
-//
+// DEFININDO ENDPOINTS BASE DAS ROTAS
 app.use("/inovacoes", inovacoes);
 app.use("/startups", startups);
 app.use("/comoAjudar", comoAjudar);
 
-//
+// INICIALIZANDO API
 app.listen(PORT, () => {
     console.log(`API de Ecotecnologias rodando em http://localhost:${PORT}`);
 });
